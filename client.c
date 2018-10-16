2d2b8b9fa3c377c696d25008f72268ec6ddd2432
Yousef Alabsi <39275070+yabsi@users.noreply.github.com>
2018-10-15 22:06:59 -0500

    Update client.c
    Fixed the while loop

--- client.c
+++ client.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define CHARMAX          1024 // maximum chars in a bufferfer
+#define CHARMAX          1024 // maximum chars in a buffer
 int port = 1234;
 
 int main(int argc, char *argv[])
@@ -21,8 +21,9 @@ int main(int argc, char *argv[])
 
   int sock;
   char *string;
-  char buffer[1025];
-  int bytesRead;
+  char buf[1025];
+  int nread;
+  int flag;
 
   if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
@@ -30,11 +31,12 @@ int main(int argc, char *argv[])
       return 1;
   }
 
-  /*
-   *  Based the initialization of the socket off
-   *  http://space.wccnet.edu/~chasselb/linux275/ClassNotes/sockets/sbasics.htm
-   *
-   */
+   /*
+    *  Based the initialization of the socket off
+    *  http://space.wccnet.edu/~chasselb/linux275/ClassNotes/sockets/sbasics.htm
+    *
+    */
+
   struct sockaddr_in server_address = { 0 };
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(port);
@@ -46,30 +48,28 @@ int main(int argc, char *argv[])
   };
 
   while (1) {
-      // if we can't copy the input to buffer or
+      // if we can't copy the input to buf or
       // if we can't write to the socket
       // or if the command was quit
-      if (fgets(buffer,CHARMAX,stdin) == NULL ||
-           write(sock, buffer, strlen(buffer)) < 0 ||
-           strcmp(buffer,"quit\n") == 0)
+      if (fgets(buf,CHARMAX,stdin) == NULL ||
+           write(sock, buf, strlen(buf)) < 0 ||
+           strcmp(buf,"quit\n") == 0)
           break;
 
-      //as long as there's more output to read
-      while (string != NULL) {
+      do {
           // bytesRead indicated how how many bytes read
-          bytesRead = read(sock, buffer, 1024);
-
+          nread = read(sock, buf, 1024);
           // if there's an error or if we're done, break
-          if (bytesRead <= 0)
+          if (nread <= 0)
               break;
-
           //looks for a data segement
-          string = memchr(buffer, 16 , bytesRead);
+          string = memchr(buf, 16 , nread);
           //writes the string with the proper indexes
-          write(1,buffer, string != NULL ? string - buffer : bytesRead);
-      }
+          write(1,buf, string != NULL ? string - buf : nread);
+      }while(string == NULL);
   }
   //closes socket with server
   close(sock);
   return 0;
+
 }
