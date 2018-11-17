bc6d09d59a9919a920748210ecad1bbc9a0b58eb
yalabsi <yousef.absi@gmail.com>
2018-11-16 23:45:25 -0600

    Added last functionality

--- server.c
+++ server.c
@@ -13,17 +13,17 @@
 
 int port = 1234;
 char* history[10];
-int hist_track =0; 
+int hist_track =0;
 
 char * displayHistory()
 {
-    char* string_history = malloc(10240); 
+    char* string_history = malloc(10240);
 
     fprintf(stdout, "%s", string_history);
     sprintf(string_history, "%s %s", string_history, "echo \"Shell command history:\n");
 
 
-    if(hist_track == 0) 
+    if(hist_track == 0)
     	sprintf(string_history, "%s %s", string_history, "There's no commands in history yet\n");
 
     if(hist_track >= 10) {
@@ -37,11 +37,11 @@ char * displayHistory()
 	    for(i=0; i< hist_track; i++)
 		    sprintf( string_history, "%s %s", string_history, history[i]);
    }
-   
+
    sprintf(string_history, "%s %s", string_history, "\"");
 
    return string_history;
-} 
+}
 
 int main(int argc, char *argv[])
 {
@@ -100,29 +100,51 @@ int main(int argc, char *argv[])
           // to quit the program
           if (fgets(buf,CHARMAX,temp2) == NULL)
               break;
-	  
+
            int i;
 	   for(i=0; i<1024; i++)
 		if(buf[i] == '\n' || buf[i] == '\0')
 			break;
 		else
 	 		buf[i] = buf[i] - 5;
-         
+
 	 if(strstr(buf,"history") != NULL) {
 		 fprintf(stdout, "%s", displayHistory());
 	  	 temp1 = popen(displayHistory(), "r");
-         }else {
+   }else if(strstr(buf,"!!") != NULL){
+     temp1 = popen(history[hist_track % 10], "r");
+   }else if(strstr(buf,"!10") != NULL){
+     temp1 = popen(history[(hist_track % 10)], "r"); // 10
+   }else if(strstr(buf,"!1") != NULL){
+     temp1 = popen(history[(hist_track+1) % 10], "r");
+   }else if(strstr(buf,"!2") != NULL){
+     temp1 = popen(history[(hist_track+2) % 10], "r");
+   }else if(strstr(buf,"!3") != NULL){
+     temp1 = popen(history[(hist_track+3) % 10], "r");
+   }else if(strstr(buf,"!4") != NULL){
+     temp1 = popen(history[(hist_track+4) % 10], "r");
+   }else if(strstr(buf,"!5") != NULL){
+     temp1 = popen(history[(hist_track+5) % 10], "r");
+   }else if(strstr(buf,"!6") != NULL){
+     temp1 = popen(history[(hist_track+6) % 10], "r");
+   }else if(strstr(buf,"!7") != NULL){
+     temp1 = popen(history[(hist_track+7) % 10], "r");
+   }else if(strstr(buf,"!8") != NULL){
+     temp1 = popen(history[(hist_track+8) % 10], "r");
+   }else if(strstr(buf,"!9") != NULL){
+     temp1 = popen(history[(hist_track+9) % 10], "r");
+   }else{
 	  temp1 = popen(buf, "r");
 	 }
           // popen runs the shell command and stores the output
-         history[hist_track % 10] = strdup(buf); 
-         hist_track++; 
+         history[hist_track % 10] = strdup(buf);
+         hist_track++;
 
 
 
           if (temp1 == NULL)
             printf("Error");
-          
+
           while (1) {
               // if there was an error somehow with copying the stream
               if (fgets(command, CHARMAX, temp1) == NULL)
